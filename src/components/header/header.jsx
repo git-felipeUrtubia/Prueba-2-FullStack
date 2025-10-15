f6c852bc454dcfbeebaf8941829bab627056a02d
git-felipeUrtubia <felipe.urtubia7@gmail.com>
2025-10-15 03:01:09 -0300

    Seccion Header terminada, falta logica

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -3,11 +3,23 @@
 import { ShoppingCart } from 'lucide-react'
 import { ChevronDown } from 'lucide-react';
 import '../../assets/styles/header.css';
-
-
-
+import { useEffect, useState } from 'react';
 
 export const Header = () => {
+
+    const [btnCategoria, setBtnCategoria] = useState(0);
+    const [rowRotate, setRowRotate] = useState(0);
+
+    useEffect(() => {
+        document.documentElement.style.setProperty("--btnCategoria", btnCategoria)
+    },[btnCategoria, rowRotate])
+
+    const handleBtnCategoria = () => {
+        setBtnCategoria(prev => (prev == 0 ? 1 : 0))
+        setRowRotate(prev => (prev == 0 ? 180 : 0))
+    }
+
+
     return (
         <div className='contain-header'>
 
@@ -18,17 +30,41 @@ export const Header = () => {
                     <button className='btn-buscar'>Buscar</button>
                 </div>
                 <div className='contain-buttons-sesion'>
-                    <button>Iniciar Sesión</button>
-                    <button>Crear Cuenta</button>
+                    <button className='btn btn-outline-primary'>Iniciar Sesión</button>
+                    <button className='btn btn-outline-primary'>Crear Cuenta</button>
                 </div>
             </div>
 
+
+
+
             <div className='contain-botones-header'>
                 <button className='btn home'><b>Home</b></button>
-                <button className='btn categorias'>
-                    <span style={{color: "gray"}}>Categorias </span>
-                    <span><ChevronDown style={{color: "grey"}}/></span>
-                </button>
+
+                
+                <div className='contain-btn-categoria'>
+                    <button className='btn categorias' onClick={handleBtnCategoria}>
+                        <span style={{color: "gray"}}>Categorias </span>
+                        <span><ChevronDown style={{
+                            color: "grey", 
+                            transform: `rotate(${rowRotate}deg)`,
+                            transition: ".3s ease"
+                        }}/></span>
+                    </button>
+                    <div className='options'>
+                        <div className='option 1'>
+                            <span>Opcion 1</span>
+                        </div>
+                        <div className='option 2'>
+                            <span>Opcion 2</span>
+                        </div>
+                        <div className='option 3'>
+                            <span>Opcion 3</span>
+                        </div>
+                    </div>
+                </div>
+
+
                 <button className='btn ofertas' style={{color: "gray"}}>Ofertas</button>
                 <button className='btn nosotros' style={{color: "gray"}}>Nosotros</button>
                 <button className='btn blog' style={{color: "gray"}}>Blog</button>
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.min.js'
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
